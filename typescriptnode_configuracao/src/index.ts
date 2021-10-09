e4dbbbdf40052cb9053ef4208bdcc9227f29517c
Nicolás Gonçalves Batista da Silva <nicolasgbdas@gmail.com>
2021-10-09 18:15:00 -0300

    Exercício1

--- typescriptnode_configuracao/src/index.ts
+++ typescriptnode_configuracao/src/index.ts
@@ -1,2 +1,18 @@
-let saudacao: string = 'Alô, mundo!';
-console.log(saudacao);
+//1. Escreva um trecho de código para imprimir os números 
+//pares definidos por um intervalo de valores inteiros não-negativos inicio e fim. Utilize versões com for e while.
+
+let ini: number = 0;
+let fim: number = 22;
+
+for (ini = 0; ini < fim; ini++) {
+    if (ini > 1 && ini % 2 == 0) {
+        console.log(ini)
+    }
+}
+while (ini < fim) {
+    if (ini % 2 == 0) {
+        console.log(ini + " é par")
+        ini++
+    }
+    ini++
+}
